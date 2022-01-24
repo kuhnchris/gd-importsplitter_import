1a7d6eb0ecb986dc45faea611c2df5c8392ecc65
Kuhn Chris <kuhnchris+github@kuhnchris.eu>
2022-01-24 18:54:51 +0100

    items now get properly split; no copies & empties

--- import_plugin.gd
+++ import_plugin.gd
@@ -30,68 +30,66 @@ func get_import_options(preset):
 					]
 		_:
 			return []
-			
+
 func get_option_visibility(_option, _options):
 	return true
-	
+
 func get_importer_name():
 	return "kuhnchris.multiteximp"
 
 func get_visible_name():
 	return "Split into AtlasTextures"
-	
+
 func get_recognized_extensions():
 	return ["png"]
 
 func get_save_extension():
 	return "tres"
-	
+
 func get_resource_type():
 	return "AtlasTexture"
 
 func import(source_file, save_path, options, _r_platform_variants, r_gen_files):
-	print(source_file)
 	var newTargetSplitBase = source_file.get_basename()
-	print(newTargetSplitBase)
-	var my_file = File.new()
-	my_file.open(source_file, File.READ)
-	var file_size = my_file.get_len()
-	var mem_temp = my_file.get_buffer(file_size)
-	my_file.close()
 
-	var img = Image.new()
-	img.load_png_from_buffer(mem_temp)
+	var srcTexture: ImageTexture = load(source_file) as ImageTexture
+	if srcTexture == null:
+		print("could not open source file: ",source_file)
+		return false
 
-	var texture = ImageTexture.new()
-	texture.create_from_image(img, 0)
+	var hSplit = srcTexture.get_width() / options.Columns
+	var vSplit = srcTexture.get_height() / options.Rows
 
-	
-	var hSplit = texture.get_width() / options.Columns
-	var vSplit = texture.get_height() / options.Rows
+	print_debug(source_file," - importing options: ",options)
 
-
-	var u = load(source_file)
-	
-	#var image = Image.new()
-	#var errimg = image.load(source_file)
-	#var texture = ImageTexture.new()
-	#texture.create_from_image(image, 0)
-
-	#var sourceTex = StreamTexture.new()
-	#sourceTex.load(source_file)	
-	
-	print_debug(options)
 	for i in range(0,options.Columns):
 		for j in range(0,options.Rows):
 			var splitObjName = "%s.x%s_y%s.%s" % [newTargetSplitBase, str(i), str(j), get_save_extension()]
 			var splitObj = AtlasTexture.new()
-			splitObj.atlas = u
+			splitObj.atlas = srcTexture
 			splitObj.region = Rect2(i*hSplit,j*vSplit,hSplit,vSplit)
-			#print_debug(splitObjName)
-			var err = ResourceSaver.save(splitObjName, splitObj)
-			if err != OK:
-				print_debug(err,splitObjName)
-				return err
-			r_gen_files.push_back(splitObjName)
+			#print_debug()
+			var pData = splitObj.get_data().data.data
+			var buff = -1
+			var allTheSame = true
+			for ii in pData.size():
+				if buff == -1:
+					buff = pData[ii]
+				else:
+					if buff != pData[ii]:
+						allTheSame = false
+			if !allTheSame:
+				var err = ResourceSaver.save(splitObjName, splitObj)
+				if err != OK:
+					print_debug(err,splitObjName)
+					return err
+				r_gen_files.push_back(splitObjName)
+			else:
+				print("skipping ",splitObjName," due to all the same color.")
+				var existingFile = Directory.new()
+				if existingFile.file_exists(splitObjName):
+					print("remove existing file...")
+					var err = existingFile.remove(splitObjName)
+
 	var atlasName = "%s.%s" % [save_path, get_save_extension()]
-	return ResourceSaver.save(atlasName, texture)
+	return ResourceSaver.save(atlasName, srcTexture)
